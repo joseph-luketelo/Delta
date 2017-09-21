7b77b7c970046c05c68d0febd4f63ea88f7611d7
dustymethod <dustymethod@users.noreply.github.com>
2017-09-20 22:37:55 -0700

    Update documentation

--- framework/EventFilter.js
+++ framework/EventFilter.js
@@ -1,5 +1,14 @@
-//event filter enum
-//EventListeners can listen for specific events, which are organized by filter.
+/*
+	An enum of Event filters. Events are organized by filter. EventListeners can
+	listen for specific events by specifying a filter to use.
+	
+	Use:
+	the ListenerMap class is a Map of an EventListener array to a filter:
+	Map<EventFilter, EventListener[]> //java
+	classes responsible for passing events to EventListeners (such as GameState)
+	may use a ListenerMap in order to send Events to the relevant listeners.
+*/
+
 let EventFilter = {
 	KEYBOARD: "KEYBOARD",
 	COLLISION: "COLLISION",
