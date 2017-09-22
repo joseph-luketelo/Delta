7811f2b08aca9e8871b5bfb2d1764b3139706cba
dustymethod <dustymethod@users.noreply.github.com>
2017-09-22 10:51:34 -0700

    Update methods

--- framework/GameState.js
+++ framework/GameState.js
@@ -1,30 +1,40 @@
 /*
-	A base State class, subclasses are used by the GameEngine.
+	A base State class, subclasses are used by the GameEngine. GameStates allow
+	the GameEngine to have different behaviours depending on the game state,
+	ex. start menu, paused, playing, game over, etc.
 	A GameState is responsible for managing an event queue, and passing events 
 	from the queue to its known EventListeners.
-	Extend to create different states (ex playing, paused, start menu, game over)
+	Since each GameState has its own EventQueue, this prevents Events from 
+	disappearing if the game is paused, and allows us to ignore input events
+	if the game is paused.
+	Extend this class to create different states (ex playing, paused, start menu, game over)
 */
 class GameState extends State {
 	constructor() {
 		super();
 		this.eventQueue = new EventQueue();
-		this.listeners = new ListenerMap();
-	}
-	
-	clearEvents() {
-		this.eventQueue.clear();
+		this.eventListeners = new ListenerMap();
 	}
 	
 	receiveEvent(e) {
 		this.eventQueue.enqueue(e);
 	}
 	
-	// add all listeners on each game object in gameObjects to this.listeners
-	addListeners(gameObjects) {
+	clearEvents() {
+		this.eventQueue.clear();
+	}
+	
+	addListener(l) {
+		this.eventListeners.addListener(l);
+	}
+	
+	// Adds all listeners on each game object in gameObjects to this.listeners
+	// @param gameObjects: an array of GameObjects
+	addListenersFromObjects(gameObjects) {
 		for (let obj of gameObjects) {
 			let objListeners = obj.getListeners();
-			for (let l of objListeners) { //TODO check for undefined?
-				this.listeners.addListener(l);
+			for (let l of objListeners) { //check for undefined?
+				this.eventListeners.addListener(l);
 			}
 		}
 	}
