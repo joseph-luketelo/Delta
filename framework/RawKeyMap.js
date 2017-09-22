312370d6e7347adeef3a02887ea93c0019314498
dustymethod <dustymethod@users.noreply.github.com>
2017-09-22 08:55:13 -0700

    Add files via upload

---
+++ framework/RawKeyMap.js
@@ -0,0 +1,7 @@
+//Maps KeyboardEvents from the browser into Events.
+class RawKeyMap {
+	static map(keyEvent) {
+		//TODO map KeyboardEvents from the browser to the appropriate Event.
+		return new Event();
+	}
+}
