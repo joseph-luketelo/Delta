9c17767c49392fbdafd0d9e609dadd6ce019f79f
dustymethod <dustymethod@users.noreply.github.com>
2017-09-22 09:05:14 -0700

    Update documentation

--- framework/KeyState.js
+++ framework/KeyState.js
@@ -1,7 +1,11 @@
-// //Singleton. Responsible for keeping track of if a key is being held up or down.
+//Responsible for keeping track of whether a key is being held up or down.
+//The GameEngine owns an instance of this class.
 
 class KeyState {
 	constructor() {
+		//an array of keys we want to keep track of.
+		//values should match key names in KeyboardEvent.key
+		//TODO documentation link here
 		this.keys = ["w", "a", "s", "d"];
 		this.keyMap = new Map();
 		for (let k of this.keys) {
@@ -18,6 +22,7 @@ class KeyState {
 	}
 	
 	//get whether a key is down or up
+	//@param key: a string containing one of the values in this.keys array.
 	getKey(key) {
 		return this.keyMap.get(key);	
 	}
