6acb0e07d69d04b3fcd741725c6cdef6262c1657
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:41:25 -0700

    Update main.js

--- framework/main.js
+++ framework/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
 /*
-	Entry and main loop methods.
+	Set up global references to html elements, like the canvas and context
+	Contains entry and main loop methods.
 */
 
 
@@ -15,7 +16,7 @@ const ENGINE = new GameEngine();
 //entry method: called by the html page, after it's is done loading (script tags must use the "defer" attribute)
 function setup() {
 	console.log("setup");
-	
+
 	//add key event listeners
 	document.addEventListener("keydown", function(event) {
 	   ENGINE.receiveRawKeyEvent(event, true);
@@ -23,7 +24,7 @@ function setup() {
 	document.addEventListener("keyup", function(event) {
 	   ENGINE.receiveRawKeyEvent(event, false);
 	});
-	
+
 	test(); //for testing
 	window.requestAnimationFrame(update); //starts the game update loop
 }
@@ -35,23 +36,9 @@ function update() {
 	window.requestAnimationFrame(update); //continue looping
 }
 
+class M {
+}
+
 //for testing
 function test() {
-	
-	let map = new ListenerMap();
-	let el = new EventListener(EventFilter.OTHER, function(e) {
-		console.log(e);
-	});
-	map.addListener(el);
-	
-	let evt = EventEnum.GAME_EVENT_DOOR_OPEN;
-	switch (evt) {
-		case EventEnum.GAME_EVENT_DOOR_OPEN:
-			// console.log("door");
-			break;
-		default:
-			console.log("default");
-			break;
-	}
-	// console.log(ENGINE.getKeyState().getKey('w'));
 }
