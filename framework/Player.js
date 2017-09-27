0d2bd710f7a4a17a7813dcfbe71c9e4e0f69d952
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:14:50 -0700

    Update Player.js

--- framework/Player.js
+++ framework/Player.js
@@ -1,8 +1,7 @@
 /*
-	A simple Player that can move on the screen.
-	See GameObject.js for more details
+	A simple Player that can move horizontally on the screen.
 	(use a and d keys)
-
+	See GameObject.js for more details
 */
 
 class Player extends GameObject {
@@ -10,32 +9,34 @@ class Player extends GameObject {
 		super();
 		this.location = new Point(x, y);
 		this.speed = 2;
-		
-		// add event listeners if needed. example:
+
+		// add event listeners here if needed. example:
 		// this.gameEventHandler = new EventListener(EventFilter.GAME_EVENT, function(e) {
 		//	handle game event
 		// });
-		// this.eventListeners.push(this.keyEventHandler);
-		
-		// Add player states if needed. ex walking, running, attacking, shooting
-		// this.currentState = new State();
-		
+		// this.addEventListener(this.keyEventHandler);
+
+		// Add player states if needed. ex walking, running, attacking, shooting, taking damage
 	}
 	update() {
-		//TODO possible optimization, set up reference to ENGINE's KeyState in constructor.
+		//NOTE possible optimization, set up reference to ENGINE's KeyState in constructor.
 		if (ENGINE.getKeyState().getKey('a')) {
 			this.move(-this.speed, 0);
 		}
 		if (ENGINE.getKeyState().getKey('d')) {
 			this.move(this.speed, 0);
 		}
-		
 	}
 	move(x, y) {
 		this.location.add(x, y);
 	}
+
 	render() {
+		//TODO render player here. currently just a blue square.
 		CTX.fillStyle = Colors.BLUE;
 		CTX.fillRect(this.location.getX(), this.location.getY(), 10, 10);
 	}
+
+	getX() { return this.location.getX(); }
+	getY() { return this.location.getY(); }
 }
