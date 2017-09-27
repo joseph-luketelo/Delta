7901c9921e65bc44c641b2b95b78e65c5bc8d81e
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:09:09 -0700

    Update GameState.js
    Update documentation
    Added dequeueEvent method

--- framework/GameState.js
+++ framework/GameState.js
@@ -1,41 +1,63 @@
 /*
-	A base State class, subclasses are used by the GameEngine. GameStates allow
-	the GameEngine to have different behaviours depending on the game state,
+	GameStates allow the GameEngine to have different behaviours depending on the game state,
 	ex. start menu, paused, playing, game over, etc.
-	A GameState is responsible for managing an event queue, and passing events 
-	from the queue to its known EventListeners.
-	Since each GameState has its own EventQueue, this prevents Events from 
-	disappearing if the game is paused, and allows us to ignore input events
-	if the game is paused.
-	Extend this class to create different states (ex playing, paused, start menu, game over)
+	Extend this class to create different states.
+
+	Responsible for managing an EventQueue, and passing Events to EventListeners.
+		Since each GameState has its own EventQueue, this prevents Events from
+		disappearing if the game is paused, and allows prevents input events
+		from piling up while the game is paused.
 */
 class GameState extends State {
 	constructor() {
 		super();
 		this.eventQueue = new EventQueue();
-		this.eventListeners = new ListenerMap();
+		this.listenerMap = new ListenerMap();
+		this.systems = new Array();
 	}
-	
-	receiveEvent(e) {
+
+	update() {}
+	render() {}
+	onEnter() {} //entry logic, ex. start a timer, spawn stuff
+	onExit() {} //exit logic, ex stop a timer, release resources
+
+	enqueueEvent(e) {
 		this.eventQueue.enqueue(e);
 	}
-	
-	clearEvents() {
-		this.eventQueue.clear();
-	}
-	
-	addListener(l) {
-		this.eventListeners.addListener(l);
-	}
-	
-	// Adds all listeners on each game object in gameObjects to this.listeners
-	// @param gameObjects: an array of GameObjects
-	addListenersFromObjects(gameObjects) {
-		for (let obj of gameObjects) {
-			let objListeners = obj.getListeners();
-			for (let l of objListeners) { //check for undefined?
-				this.eventListeners.addListener(l);
+
+	dequeueEvent() {
+		while (!this.eventQueue.isEmpty()) {
+			const e = this.eventQueue.dequeue();
+			const listeners = this.listenerMap.getEventListeners(e.getEventFilter());
+			for (let l of listeners) {
+				l.handleEvent(e);
 			}
 		}
+	}
+
+	//@param system a System
+	addSystem(system) {
+		if (system instanceof System == false) { throw new TypeError("invalid argument:" + system); }
+		this.systems.push(system);
+		// this.registerEventListeners(system.getEventListeners());
+	}
+
+	//@param l: an EventListener
+	registerEventListener(l) {
+		if (l instanceof EventListener == false) { throw new TypeError(); }
+		this.listenerMap.addEventListener(l);
+	}
+
+	//@param listeners: an Array of EventListeners
+	registerEventListeners(listeners) {
+		if (listeners instanceof Array == false) { throw new TypeError(); }
+		for (let l of listeners) {
+			if (l instanceof EventListener == false) { throw new TypeError(); }
+			this.listenerMap.addEventListener(l);
+		}
+	}
+
+	clearEvents() {
+		this.eventQueue.clear();
 	}
 }
