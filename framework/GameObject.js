b578465bb9808828b5a7c4825b4b4d0b455afe76
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:08:16 -0700

    Update GameObject.js

--- framework/GameObject.js
+++ framework/GameObject.js
@@ -1,38 +1,48 @@
 /*
-	A base game object class for things like Players and Enemies.
-	Contains an array of EventListeners. Subclasses define their
-	own EventListeners, and should add them to the array.
-	Contains an EventPublisher, should the Subclass want to publish
-	an event.
-	
-	// ex. Player creates a FlashBang event
+	Base class for game objects like Player, Enemy, Asteroid, etc.
+
+	Contains an EventPublisher, for if the Subclass wants to publish Events.
+	// ex. Player uses flashbang and creates a FlashBang event
 	flashbang() {
-		let bang = new Event(EventFilter.COMBAT, EVENT_ENUM.FLASH_BANG, null);
+		let bang = new Event(EventFilter.COMBAT, EventEnum.FLASH_BANG, null);
 		this.publishEvent(bang);
 	}
-	
 */
 
 class GameObject {
 	constructor() {
+		this.isActive = true;
 		this.eventListeners = new Array();
 		this.eventPublisher = new EventPublisher();
 	}
-	
-	//Override for specific behaviours.
+
+	// Override for specific behaviours.
 	update() {}
 	render() {}
-	
-	//Return this object's array of EventListeners. Used by GameStates,
-	// for adding GameObject's listeners to the GameState's ListenerMap.
-	getListeners() {
-		return this.eventListeners;
-	}
-	
-	//Publish an event to the current EventQueue in the GameEngine.
+
+	// Publish an event to the current EventQueue.
+	// @param e an Event to publish
 	publishEvent(e) {
+		if (e instanceof Event == false) { throw new TypeError(); }
 		this.eventPublisher.publishEvent(e);
 	}
+
+	getIsActive() {
+		return this.isActive;
+	}
+
+	//deactivate, possibly flag for removal
+	deactivate() {
+		this.isActive = false;
+	}
+
+	addEventListener(eventListener) {
+		if (eventListener instanceof EventListener == false) { throw new TypeError(); }
+		this.eventListeners.push(eventListener);
+	}
+
+	// Return this object's array of EventListeners.
+	getEventListeners() {
+		return this.eventListeners;
+	}
 }
-
-
