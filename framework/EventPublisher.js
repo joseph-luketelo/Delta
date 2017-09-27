d22dd3620a54c150d9f00109723581e9a7c3b50d
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:03:25 -0700

    Update documentation

--- framework/EventPublisher.js
+++ framework/EventPublisher.js
@@ -1,5 +1,5 @@
 /*
-	Publishes events to the EventQueue of the current game state.
+	Publishes events to the EventQueue of the game engine's current game state.
 	Objects that want to create Events will need an EventPublisher.
 	References global GameEngine ENGINE. not great practice, open to suggestions
 */
