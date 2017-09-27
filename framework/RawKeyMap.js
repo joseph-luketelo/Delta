8b05d4321a701ea84534b7ccfb0b989bc56001f8
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:25:40 -0700

    Update RawKeyMap.js

--- framework/RawKeyMap.js
+++ framework/RawKeyMap.js
@@ -1,7 +1,9 @@
 //Maps KeyboardEvents from the browser into Events.
+//only map desired KeyboardEvents.
 class RawKeyMap {
 	static map(keyEvent) {
 		//TODO map KeyboardEvents from the browser to the appropriate Event.
 		return new Event();
+		//TODO return null for undesired key events.
 	}
 }
