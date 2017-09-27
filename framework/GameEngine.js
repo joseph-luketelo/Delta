73e416c73b8311d56285b28f0f4e7c439c5e4478
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:06:25 -0700

    Update GameEngine.js

--- framework/GameEngine.js
+++ framework/GameEngine.js
@@ -8,60 +8,68 @@
 class GameEngine {
 	constructor() {
 		this.keyState = new KeyState(); //keeps track of which keys are down
-		
-		//TODO define game objects here, ex player, enemies, platforms, example:
+		this.playingState = new PlayingGameState(); //the main GameState
+		// this.startMenuState = new GameState(); //TODO
+		// this.pausedState = new GameState(); //TODO
+		// this.gameOverState = new GameState(); //TODO
+
+		//Define game objects here, ex player, enemies, platforms, example:
 		this.player = new Player(WIDTH/2, HEIGHT/2);
-		this.gameObjects = new Array(); //list of all
-		this.gameObjects.push(this.player);
-		
-		//TODO define other states, such as playing, game over, etc
-		//pass GameObjects to the states that should know about them. 
-		// ex a PlayingState should know about the Player, a PauseState shouldn't.
-		this.testGameState = new TestGameState(this.gameObjects); //pass all objects to this state.
-		
-		this.currentState = this.testGameState; //set the current state.
+		this.asteroids = new Array(); //array of asteroids
+		// this.gameObjects = new Array();
+
+		//Define your System here. pass objects defined above to your system's constructor.
+		//let yourSystem = new SomeKindOfSystem(relevantObjects); //example
+		let playerSystem = new PlayerSystem(this.player);
+		let collisionSystem = new CollisionSystem(this.player, this.asteroids);
+
+		//Add your system to the main playingState
+		// this.playingState.addSystem(yourSystem); //example
+		this.playingState.addSystem(playerSystem);
+
+		//Register event listeners from GameObjects and Systems with approrpiate GameState
+		//ex typically playingState, but GameObjects relating to the pause menu would register to the pausedState.
+		this.playingState.registerEventListeners(this.player.getEventListeners());
+
+		//set initial game state and enter
+		this.currentState = this.playingState; //set the current state.
 		this.currentState.onEnter();
-		
 	}
-	
+
 	update() {
 		this.currentState.update();
 	}
-	
+
 	render() {
 		this.currentState.render();
 	}
-	
+
 	//receive Events, and pass to the current state to handle.
 	receiveEvent(e) {
-		// console.log(e);
-		if (e instanceof Event) {
-			this.currentState.receiveEvent(e);
-		} else {
-			throw new TypeError(e + " is not an Event.");
-		}
-		
+		if (e instanceof Event == false) { throw new TypeError(); }
+		this.currentState.enqueueEvent(e);
 	}
 
-	//transition from the current state to a new state, ex from the start menu to the playing state.
+	// Switch to a new state, ex from the start menu to the playing state.
 	enterState(newState) {
+		if (newState instanceof State == false) { throw new TypeError(); }
 		this.currentState.onExit();
 		this.currentState = newState;
 		this.currentState.onEnter();
 	}
-	
-	//receive a KeyboardEvent from the browser, 
-	//translate the KeyboardEvent into an Event, and queue it,
-	//and update the KeyState.
+
+	// Translates the given KeyboardEvent into an Event, queues it,
+	// and updates this game engine's keyState.
+	// @param e: a KeyboardEvent from the browser
+	// @param isPressed: a boolean representing whether the KeyboardEvent
+	// corresponded to a pressed or released event.
 	receiveRawKeyEvent(e, isPressed) {
 		this.receiveEvent(RawKeyMap.map(e));
 		this.keyState.setKey(e.key, isPressed);
 	}
-	
+
 	//accessed by objects who want to know about KeyStates, ex Player.
 	getKeyState() {
 		return this.keyState;
 	}
 }
-
-
