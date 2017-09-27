ab4391ddcfc583db575b4ab2763c963cc21b4157
dustymethod <dustymethod@users.noreply.github.com>
2017-09-26 21:02:44 -0700

    Update documentation
    Update documentation and added valid arg check to constructor.

--- framework/EventListener.js
+++ framework/EventListener.js
@@ -1,37 +1,45 @@
 /*
-	Class used for handling events from a specific filter.
-	An EventListener chooses a filter for the Events it wants to receive.
-	In the constructor, An EventListener also takes a method, that is called
-	when an event is received.
-	
-	// example
-	//create the listener
+	Class used for handling Events.
+	An EventListener chooses a filter for the Events it wants to handle.
+	Must be registered to a GameState in order to receive events from an EventQueue.
+
+	// Example & Usage:
+
+	//create listener
 	let doorListener = new EventListener(EventFilter.GAME_EVENT, function(doorEvent) {
-		console.log("a door was opened " + doorEvent);
-	);
-	
-	//add it to the owning object's list. GameObjects and GameStates have lists of EventListeners.
-	this.eventListeners.addListener(doorListener);
-	
+		console.log("a door was opened.");
+	});
+
+	// Add the listener
+	// if the listener belongs to a GameObject or System, add it to the GameObject's list of listeners.
+	object.addEventListener(doorListener); // A) add externally from game engine's constructor
+	this.addEventListener(doorListener);   // B) or add internally within the object's constructor
+
+	// Registerh the listener(s)
+	// register the gameObject's/state's listeners to a GameState for it to be notified of events:
+	gameState.registerEventListeners(object.getEventListeners());
+	gameState.registerEventListener(doorListener); //standalone event listener:
+
 */
+
 class EventListener {
 	// @param filter: the EventFilter this class is interested in.
-	// @param eventHandler: a method that takes an Event as its parameter. this
-	// method is invoked when this listener's handleEvent(e) method is called.
+	// @param eventHandler: a callback/method that takes an Event as its parameter.
+	// The given method is invoked when this listener's handleEvent(e) method is called.
 	constructor(filter, eventHandler) {
-		if (eventHandler == undefined || filter == undefined) {
-			throw new TypeError("argument error: " + " EventListener(" + eventHandler + ", " + filter + ")");
+		if (!EventFilter.hasOwnProperty("" + filter) || typeof eventHandler != "function") {
+			throw new TypeError("invalid constructor arguments");
 		}
 		this.eventHandler = eventHandler;
 		this.filter = filter;
 	}
+
 	//@param event an Event to handle
 	handleEvent(event) {
-		if (event instanceof Event) { //TODO check if event is of the correct filter?
-			this.eventHandler(event);
-		}
+		if (event instanceof Event == false) { throw new TypeError(); }
+		this.eventHandler(event);
 	}
-	
+
 	getEventFilter() {
 		return this.filter;
 	}
