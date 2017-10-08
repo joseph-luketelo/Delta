d9efe1f3cec4c83ca9ffb083b3359a1759f29aa7
dustymethod <dustymethod@users.noreply.github.com>
2017-10-07 17:18:38 -0700

    Update CollisionSystem.js

--- framework/CollisionSystem.js
+++ framework/CollisionSystem.js
@@ -1,26 +1,43 @@
 /*
 	A possible template for a CollisionSystem.
 	Handles collisions between objects like player, projectiles, and asteroids.
+	NOTE: alter as needed.
  */
+
+
 
 class CollisionSystem extends System {
 	// @param player: The player
 	// @param asteroids: An array of asteroids
-	constructor(player = null, asteroids = null) {
+	constructor(player, asteroids) {
+		if (player instanceof Player == false || asteroids instanceof Array == false) { throw new Error("Invalid arguments"); }
 		super();
+		
+		// GameObjects or lists of GameObjects that will be checked for collisions.
 		this.player = player;
 		this.asteroids = asteroids;
+		// this.projectiles = player.getProjectiles(); //get the player's list of projectiles.
 	}
+	
 	update() {
-		//TODO implement collision checking & handling
-		/*  Example
-			for each asteroid
-				if asteroid is colliding with player
-				player.damage(asteroid.density)
-				asteroid.deactivate(false);
-				//possibly publish a CollisionEvent, for other Systems interested
-				//in collisions. ex sound or particle system.
-				this.publishEvent(new CollisionEvent());
-		*/
+		checkPlayer_Asteroids();
+		checkProjectiles_Asteroids();
+	}
+	
+	// Check if player is colliding with an asteroid
+	checkPlayer_Asteroids() {
+		// TODO check collision
+		// TODO handle the collision
+	}
+	
+	// Check if any of the player's projectiles are colliding wit an asteroid
+	checkProjectiles_Asteroids() {
+		// TODO check collision
+		// TODO handle the collision
+	}
+	
+	//NOTE example method for if an asteroid is hit
+	damageAsteroid(asteroid, projectile) {
+		asteroid.damage(projectile.getDamage);
 	}
 }
