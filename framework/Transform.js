4d28ef53f24bd86f91a0f21d96582f89433d877a
dustymethod <dustymethod@users.noreply.github.com>
2017-10-07 17:49:02 -0700

    Update Transform.js

--- framework/Transform.js
+++ framework/Transform.js
@@ -1,11 +1,30 @@
 class Transform {
-	constructor() {
-		this.location = new Point();
+	constructor(x = 0, y = 0) {
+		this.location = new Point(x, y);
+		this.rotation = 0;
+	}
+	getLocation() {
+		return this.location;
+	}
+	setLocation(x, y) {
+		this.location.set(x, y);
 	}
 	getX() {
 		return this.location.getX();
 	}
 	getY() {
 		return this.location.getY();
+	}
+	setX(x) {
+		this.location.setX(x);
+	}
+	setY(y) {
+		this.location.setX(y);
+	}
+	getRotation() {
+		return this.rotation;
+	}
+	setRotation(r) {
+		this.rotation = r;
 	}
 }
