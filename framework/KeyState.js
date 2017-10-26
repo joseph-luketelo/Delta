cb4f2055bc4079f520f46fcf9e37d781089c0344
dustymethod <dustymethod@users.noreply.github.com>
2017-10-26 08:24:56 -0700

    Update KeyState.js

--- framework/KeyState.js
+++ framework/KeyState.js
@@ -3,15 +3,17 @@
 
 class KeyState {
 	constructor() {
-		//an array of keys we want to keep track of.
-		//values should match key names in KeyboardEvent.key
-		//TODO documentation link here
-		this.keys = ["w", "a", "s", "d"];
-		this.keyMap = new Map();
+		//value names should match key names in KeyboardEvent.key
+		//ref: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+		this.keys = ["w", "a", "s", "d", //an array of keys we want to keep track of.
+					"q", "e", "r", "t", "y",
+					"z", "x", "c", "v",
+					"g",
+					" ", "Shift", "Control"]; //" " is for the spacebar.
+		this.keyMap = new Map(); //Map<String, Boolean>
 		for (let k of this.keys) {
 			this.keyMap.set(k, false);
 		}
-		this.instance = null;
 
 	}
 	setKey(key, state) {
@@ -20,7 +22,8 @@ class KeyState {
 		}
 		// console.log(key, state);
 	}
-	
+
+	//set each key to false
 	clearKeys() {
 		this.keyMap.forEach(function(value, key) {
 			value = false;
@@ -29,7 +32,7 @@ class KeyState {
 
 	//get whether a key is being held down or not.
 	//@param key: a string equal to one of the values in this.keys array.
-	//@return true if the given key is down, false if it's up
+	//@return true if the given key is down, false if it"s up
 	getKey(key) {
 		return this.keyMap.get(key);
 	}
