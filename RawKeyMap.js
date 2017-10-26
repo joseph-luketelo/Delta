b7707935afb96c3563d8dddee57f13de7942ba6e
sschandi <sukhdeep.29@live.ca>
2017-10-25 23:42:06 -0700

    Delete RawKeyMap.js

--- RawKeyMap.js
+++
@@ -1,9 +0,0 @@
-//Maps KeyboardEvents from the browser into Events.
-//NOTE only map the necessary KeyboardEvents that will be used.
-class RawKeyMap {
-	static map(keyEvent) {
-		//TODO map KeyboardEvents from the browser to the appropriate Event.
-		return new Event();
-		//TODO return null for undesired key events.
-	}
-}
