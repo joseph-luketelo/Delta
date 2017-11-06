8192c3fb3b08ae29bf24fa64c5c5d8d4bfd43fe5
Ash-117 <32125521+Ash-117@users.noreply.github.com>
2017-11-06 10:43:55 -0800

    Create Sound.js

---
+++ framework/Sound.js
@@ -0,0 +1,49 @@
+//This class is for Sound Effects
+//Work in progress
+
+class SoundSystem extends System {
+	  constructor() {
+		  this.backgroundSong    = new Audio('assets/AsteroidThemeSong.mp3');
+		  this.asteroidExplosion = new Audio('assets/AsteroidExplosion.mp3');
+		  this.alienBossTheme    = new Audio('assets/AlienBoss.mp3');
+		  this.bulletSound       = new Audio('assets/LaserBlaster.mp3');
+		  this.thrusterSound     = new Audio('assets/Thrusters.mp3');
+		
+		  let wKeyListener = new EventListener(EventFilter.KEYBOARD, function(event) {
+			  if (event.getEventEnum() == EventEnum.keydown_w) {
+				  playThrusterSound();
+			  }
+		  });
+		  this.addEventListener(wKeyListener);
+		
+		  let sKeyListener = new EventListener(EventFilter.KEYBOARD, function(event) {
+			  if (event.getEventEnum() == EventEnum.keydown_s) {
+				  playThrusterSound();
+			  }
+		  });
+		  this.addEventListener(sKeyListener);
+	}
+
+	  playBackgroundSong(){
+		  this.backgroundSong.play();
+		  this.backgroundSong.loop = true;
+	  }
+
+	  playAsteroidExplosion(){
+		  this.asteroidExplosion.play();
+	  }
+
+	  playAlienBossTheme(){
+		  this.alienBossTheme.play()
+	  	this.alienBossTheme.loop = true;
+	  }
+
+	  playBulletSound(){
+		  this.bulletSound.play();
+	  }
+
+	  playThrusterSound(){
+		  this.thrusterSound.play();
+  	}
+
+  }
