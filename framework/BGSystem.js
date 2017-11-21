89d20ec065ca1b64ec1008e1e2d72213e047399c
junrut <junrut@sfu.ca>
2017-11-21 10:34:44 -0800

    Change y axis initial position of background image

--- framework/BGSystem.js
+++ framework/BGSystem.js
@@ -3,8 +3,8 @@
 class BGSystem extends System {
 	constructor() {
 		super();
-		this.loc1Scroller = new Point(0, 0); // bg image location for scroller mode
-		this.loc2Scroller = new Point(900, 900); 
+		this.loc1Scroller = new Point(0, -450); // bg image location for scroller mode
+		this.loc2Scroller = new Point(900, 1350); 
 		// this.loc3Scroller = new Point(500, 500); 
 		// this.loc4Scroller = new Point(200, 200); 
 
@@ -32,11 +32,7 @@ class BGSystem extends System {
 
 	//called every frame
 	render() {
-		if(LevelPresets.getPresets()[0].mode.type === "SCROLLER") {
 			CTX.drawImage(this.bgImageScroller, this.loc1Scroller.getX(), this.loc1Scroller.getY(), this.loc2Scroller.getX(), this.loc2Scroller.getY());
-		} else if(LevelPresets.getPresets()[0].mode.type === "ASTEROID") {
-			CTX.drawImage(this.bgImageAsteroid, this.loc1Asteroid.getX(), this.loc1Asteroid.getY(), this.loc2Asteroid.getX(), this.loc2Asteroid.getY());
-		}
 	}
 
 	// continuously move images vertically (top to bottom). called by another
@@ -44,8 +40,8 @@ class BGSystem extends System {
 	// scroll() will stop being called when game is in asteroid mode.
 	scroll() {
 		// console.log(this.loc1Scroller.getX(), this.loc1Scroller.getY());
-		if(this.loc1Scroller.getY() === 600) {
-			this.loc1Scroller.set(0, 0);
+		if(this.loc1Scroller.getY() === 0) {
+			this.loc1Scroller.set(0, -450);
 		}
 		this.loc1Scroller.addPoint(this.velocity); //move images vertically
 		// reset image locations when offscreen (move back to top, when past bottom)
